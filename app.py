8562991b9a9eba9913bdcfcfe895df7c5f20ea4a
Temmy-bit <ainatemiloluwa6@gmail.com>
2026-02-18 17:12:08 +0100

    This Is the commit for web application

---
+++ app.py
@@ -0,0 +1,25 @@
+import pickle
+from flask import Flask, request, jsonify,app,url_for,render_template
+import numpy as np
+# from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+# Load the trained model
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(np.array(list(data.values())).reshape(1, -1))
+    data_scaled = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    prediction = regmodel.predict(data_scaled)
+    print(prediction[0])
+    return jsonify({'prediction': float(prediction[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
