62af9d365c1eb13542b61aeaf913cebc06f0b53d
Temmy-bit <ainatemiloluwa6@gmail.com>
2026-02-19 09:22:43 +0100

    Web App Completion

--- app.py
+++ app.py
@@ -21,5 +21,15 @@ def predict_api():
     print(prediction[0])
     return jsonify({'prediction': float(prediction[0])})
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print(data)
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    # print(final_input)
+    output = regmodel.predict(final_input)
+    return render_template("home.html",prediction_text=f"The House Price Prediction is ${output[0].round(4)}")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
